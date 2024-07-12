7b957771e590afdede7b29e0f71e720176cbcf6c
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-07-12 10:32:57 +0530

    done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,34 +1,84 @@
 const express = require("express");
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt"); // add this line
+const bcrypt = require("bcrypt");
+const authMiddleware = require("../middlewares/authMiddleware");
+const jwt = require('jsonwebtoken')
+
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try{
-        const userExists = await User.findOne({email:req.body.email})
-        if(userExists){
-            return res.send({
-                success:false,
-                message:"User already exists"
-            })
-        }
-        const salt = await bcrypt.genSalt(10); // generate a salt
-        const hashedPassword = await bcrypt.hash(req.body.password, salt); // hash the password
-        req.body.password = hashedPassword;
-        const user = new User(req.body);
-        await user.save();
-        
-        return res.status(201).json('User Created');
-    }catch(error){
-        return res.json(error);
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+
+    if (userExists) {
+      res.send({
+        success: false,
+        message: "User Already Exists",
+      });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.send({
+      success: true,
+      message: "You've successfully signed up, please login now!",
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.post("/login", async (req, res) => {
-  
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      res.send({
+        success: false,
+        message: "User Does not exist , please register",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+
+    // const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    //   expiresIn: "1d",
+    // });
+
+    res.send({
+      success: true,
+      message: "You've successfully logged in!",
+      // token: token,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
+router.get("/get-current-user", authMiddleware, async (req, res) => {
+  const user = await User.findById(req.body.userId).select("-password");
 
-module.exports = router;
+  res.send({
+    success: true,
+    message: 'You are authorized to go to the protected route!',
+    data: user
+   })
+});
+
+module.exports = router;
